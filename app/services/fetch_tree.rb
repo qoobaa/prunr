e76518ec1463278dcf3157e56a5dc4eafc4e6a43
Kuba Kuźma <kuba@kubakuzma.com>
2018-09-11 09:56:03 +0200

    wording

--- app/services/fetch_tree.rb
+++ app/services/fetch_tree.rb
@@ -8,7 +8,7 @@ class FetchTree < ApplicationService
     retries_count ||= MAX_RETRIES
     uri = URI(API_URI % {name: name})
     response = Net::HTTP.get_response(uri)
-    raise ApiError, "API failed to respond #{MAX_RETRIES} times subsequently" if response.is_a?(Net::HTTPServerError)
+    raise ApiError, "API failed to respond #{MAX_RETRIES} times consecutively" if response.is_a?(Net::HTTPServerError)
     JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
   rescue ApiError
     retries_count -= 1
